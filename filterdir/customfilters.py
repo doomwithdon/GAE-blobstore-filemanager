7c420ac8331c338741a0d5f6539a34bc35eec515
PenguinSir2000 <penguinsir2000@gmail.com>
2014-09-23 13:08:24 +0800

    優化customfilters內容

--- filterdir/customfilters.py
+++ filterdir/customfilters.py
@@ -1,6 +1,7 @@
 # -*- coding:UTF-8 -*-
 from google.appengine.ext import webapp
 import math
+import logging
 
 register = webapp.template.create_template_register()
 
@@ -17,18 +18,23 @@ def countByte(value):
     elif 1024**3 <= value :
       return str(value / (1024**3)) + "GB"
 
-@register.filter('blob_percentage')
-def blob_percentage(value):
-	value = float(value)
-	#GAE的blobstore上限為5GB
-	value = value / 5 /(1024**3)
-	value = math.floor ( value * 10000 ) /100
-	return str(value) + "%"
-
-@register.filter('bandwidth_percentage')
-def bandwidth_percentage(value):
-  value = float(value)
-  #GAE的bandwidth上限為1GB
-  value = value / (1024**3)
-  value = math.floor ( value * 10000 ) /100
+@register.filter('percentage')
+def percentage(value,select):
+  value = float(value)  
+  value = value / (1024**3)  #先將數值轉換至GB為單位
+  if select =="Blob" :
+    value = value / 5 #blobstore上限為5GB
+  elif select =="bandwidth" :
+    value = value / 1 #bandwidth上限為1GB
+  value =  math_chop(value)
   return str(value) + "%"
+
+#無條件捨去，只留到小數第二位
+def math_chop(value):
+  return math.floor ( value * 10000 ) /100
+
+#測試參數代入是否有效
+@register.filter('cut')
+def cut(value, arg):
+    """Removes all values of arg from the given string"""
+    return str(value) + "+" +arg
